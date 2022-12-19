ae39934d509b7c183122aee1efb705c358ee665b
Riley <94302477+Rilaey@users.noreply.github.com>
2022-12-19 13:54:45 -0500

    created files, installed packages, started creating database

---
+++ db/schema.sql
@@ -0,0 +1,23 @@
+-- drop database if it exist 
+DROP DATABASE IF EXIST Company_DB
+
+-- create database
+CREATE DATABASE Company_DB
+
+-- use database we just created
+USE Company_DB
+
+-- create department table
+CREATE TABLE department(
+
+)
+
+-- create role table
+CREATE TABLE role(
+
+)
+
+-- create employee table
+CREATE TABLE employee(
+    
+)
